41ee44ab02c37ab51784cc035f1a3ef0ba0e79dc
Eric Schmidt <ericschmidt49@gmail.com>
2024-02-01 18:41:34 -0300

    Create script to dump data into json file

---
+++ flu_data_processor_json.py
@@ -0,0 +1,83 @@
+import requests
+from datetime import datetime, timedelta
+import calendar
+import json
+
+class NotificationsProcessor:
+
+    def __init__(self):
+        # API credentials
+        self.url = "https://elasticsearch-saps.saude.gov.br/desc-esus-notifica-estado-pr/_search"
+        self.username = "user-public-notificacoes"
+        self.password = "Za4qNXdyQNSa9YaA" # standard password for the API
+        self.session = requests.Session()
+        self.session.auth = (self.username, self.password)
+
+    def fetch_and_processs_data(self, start_date, end_date):
+
+        # Define the date range
+        start_date = datetime(2022, 11, 1)
+        end_date = datetime.now()
+
+        # Define the array that will hold all the records
+        all_records = []
+
+        # Loop through each year and month
+        current_date = start_date
+        while current_date <= end_date:
+            # Calculate the last day of the current month
+            last_day = calendar.monthrange(current_date.year, current_date.month)[1]
+
+            # Define the query parameters for the search
+            query_params = {
+                "q": f"@timestamp:[{current_date.strftime('%Y-%m-%dT00:00:00.000Z')} TO {current_date.replace(day=last_day).strftime('%Y-%m-%dT23:59:59.999Z')}]",
+                "size": 10000  # Specify the batch size
+            }
+
+            # Make a GET request to the API URL with query parameters
+            response = self.session.get(self.url, params=query_params)
+
+            if response.status_code == 200:
+                # Parse the JSON response
+                data = response.json()
+                # Process the hits as needed
+                hits = data.get("hits", {}).get("hits", [])
+                for hit in hits:
+                    source = hit.get("_source", {})
+                    keys_to_retrieve = ["id", "@timestamp", "sexo", "idade", "racaCor",
+                                        "sintomas", "dataInicioSintomas",
+                                        "municipio", "estado", "dataPrimeiraDose", "dataSegundaDose",
+                                        "dataNotificacao", "municipioNotificacao", "estadoNotificacao",
+                                        "recebeuAntiviral", "profissionalSaude", "profissionalSeguranca"]
+                    selected_data = {}
+                    for key in keys_to_retrieve:
+                        value = source.get(key)
+                        selected_data[key] = value
+
+                    # Append selected data to all records list
+                    all_records.append(selected_data)
+
+                print(f"Processed {len(hits)} hits for {current_date}")
+
+            else:
+                print(f"Error in request for {current_date}: {response.status_code}")
+
+            # Move to the next month
+            current_date = (current_date.replace(day=1) + timedelta(days=32)).replace(day=1)
+
+
+        # Insert all records into json file
+        with open("flu_data.json", "a", encoding="utf-8") as json_file:
+            # Load existing data from json file
+            json.dump(all_records, json_file, indent=2, ensure_ascii=False)
+
+        print("Data insertion complete.")
+
+if __name__ == "__main__":
+
+    processor = NotificationsProcessor()
+
+    start_date = datetime(2022, 11, 1)
+    end_date = datetime.now()
+
+    processor.fetch_and_processs_data(start_date, end_date)
